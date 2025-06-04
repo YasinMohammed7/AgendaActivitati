abb5f7f5ea28adcc1649d1a2f780457628694260
Yasin Mohammed <mohammedyasinym16720@gmail.com>
2025-06-04 14:32:48 +0300

    Add project files.

---
+++ MainForm.cs
@@ -0,0 +1,15 @@
+namespace AgendaActivitati
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void activityButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello");
+        }
+    }
+}
